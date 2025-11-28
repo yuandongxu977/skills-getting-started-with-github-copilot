bef0e9f8e4f26d3dd1e27cf80020e7bd4799bfcc
yuandongxu977 <yuandong.xu@nttdata.com>
2025-11-28 15:34:45 +0800

    test

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 import os
 from pathlib import Path
-
+#test
 app = FastAPI(title="Mergington High School API",
               description="API for viewing and signing up for extracurricular activities")
 
